a4c3a80e140e5eea352c67a8ad3ac649313e7213
Rijul Kumar Srivastava <116459036+RIJUL-KUMAR-SRIVASTAVA@users.noreply.github.com>
2024-12-24 22:53:49 +0000

    code

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const account_id=1234
+let accountemail="rijul@gmail.com"
+var accountpassword="54266"
+accountcity="kolkata"
+
+// account_id=2
+accountemail="atul2@gmail.com"
+accountpassword="2456"
+accountcity="chandigarh"
+
+// preffer not to use var because of issue in block scope and functional scope{}
+
+console.table([account_id,accountemail,accountpassword,accountcity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+
+// alert( 3 + 3 ) we are using node js not browser
+
+let name="Rijul"
+let age=18
+let isloggedin=true
+let state;
+console.log(typeof"Rijul")
+console.log(typeof null)
+console.log(typeof undefined)
+// string datatypes
+// number datatypes
+// boolean datatypes
+// undefined datatypes
+// null datatypes=> standalone value 
+// symbol datatype=>unique
+// object
