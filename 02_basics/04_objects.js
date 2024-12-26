44830dd2fb3e7b377c266767f17a3b98c85b0dc4
Rijul Kumar Srivastava <116459036+RIJUL-KUMAR-SRIVASTAVA@users.noreply.github.com>
2024-12-26 07:39:48 +0000

    code

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,4 +5,40 @@ tinderuser.id="1234"
 tinderuser.name="rahul"
 tinderuser.isloggedinn=false
 
-console.log(tinderuser)
+// console.log(tinderuser)
+const regularuser = {
+    email:"some@gmail.com",
+    name:{
+        fullname:{
+            firstname:"rijul",
+            lastname:"srivastava"
+        }
+    }
+}
+// console.log(regularuser.name.fullname.firstname)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+const obj4={5:"e",6:"f"}
+const obj3=Object.assign({},obj1,obj2,obj4)//merging two object
+// console.log(obj3)
+
+obje=[{
+    id:1,
+    email:"r@gmail.com"
+},
+{
+    id:1,
+    email:"r@gmail.com"
+},
+{
+    id:1,
+    email:"r@gmail.com"
+}]
+console.log(obje[1].email)
+console.log(tinderuser);
+console.log(Object.keys(tinderuser))
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))
+
+console.log(tinderuser.hasOwnProperty("isloggedinn"))
